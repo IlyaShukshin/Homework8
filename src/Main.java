6e87519d376832e0b8c717f1ea76cdbb928b22c3
IlyaShukshin <freevhip88@mail.ru>
2024-03-09 13:08:04 +0300

    Fixed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача №1");
-        int[] level = new int[]{1, 2, 3};
+        int[] level = new int[3];
+        level[0] = 1;
+        level[1] = 2;
+        level[2] = 3;
         double[] share = {1.57, 7.654, 9.986};
         int[] mass = new int[4];
         mass[0] = 10;
@@ -11,34 +15,63 @@ public class Main {
         mass[2] = 30;
         mass[3] = 40;
         System.out.println();
-
         System.out.println("Задача №2");
-        System.out.print(level[0] + ", " + level[1] + ", " + level[2]);
+        for (int i = 0; i < level.length; i++) {
+            if (i != level.length - 1)
+                System.out.print(level[i] + ", ");
+            else {
+                System.out.print(level[i]);
+            }
+        }
         System.out.println();
-        System.out.print(share[0] + ", " + share[1] + ", " + share[2]);
+        for (int i = 0; i < share.length; i++) {
+            if (i != share.length - 1)
+                System.out.print(share[i] + ", ");
+            else {
+                System.out.print(share[i]);
+            }
+        }
         System.out.println();
-        System.out.print(mass[0] + ", " + mass[1] + ", " + mass[2] + ", " + mass[3]);
+        for (int i = 0; i < mass.length; i++) {
+            if (i != mass.length - 1)
+                System.out.print(mass[i] + ", ");
+            else {
+                System.out.print(mass[i]);
+            }
+        }
         System.out.println();
-
         System.out.println("Задача №3");
-        for (int i = 2; i >= 0; i--) {
-            System.out.print(level[i] + ", ");
+        for (int i = level.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(level[i] + ", ");
+            else {
+                System.out.print(level[i]);
+            }
         }
         System.out.println();
-        for (int i = 2; i >= 0; i--) {
-            System.out.print(share[i] + ", ");
+        for (int i = share.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(share[i] + ", ");
+            else {
+                System.out.print(share[i]);
+            }
         }
         System.out.println();
-        for (int i = 3; i >= 0; i--) {
-            System.out.print(mass[i] + ", ");
+        for (int i = mass.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(mass[i] + ", ");
+            else {
+                System.out.println(mass[i]);
+            }
         }
         System.out.println();
-
         System.out.println("Задача №4");
         for (int i = 0; i < level.length; i++) {
-            level[i]++;
             if (level[i] % 2 == 0) {
-                System.out.print(level[i] + ", ");
+                System.out.print(level[i] + " ");
+            } else {
+                level[i]++;
+                System.out.print(level[i] + " ");
             }
         }
     }
